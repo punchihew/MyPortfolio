2b5a1cd3caba9adc75bea9c66158fab0d116e20f
punchihewa <navishkapunchihewa66@gmail.com>
2023-10-08 22:50:59 +0530

    update

---
+++ assignments/JS/IN_MEMORY_POS/Controller/Place_Oder_Controller.js
@@ -0,0 +1,8 @@
+$('#customerIdTxtOrderPage').val(detailsBig[0].cid);
+$('#customerNameTxtOrderPage').val(detailsBig[0].cname);
+$('#customerAddressTxtOrderPage').val(detailsBig[0].caddress);
+$('#customerSalaryTxtOrderPage').val(detailsBig[0].csalary);
+$('#itemCodeTxtOrderPage').val(itemDetailsBig[0].itemCode);
+$('#itemNameTxtOrderPage').val(itemDetailsBig[0].itemName);
+$('#unitePriceTxtOrderPage').val(itemDetailsBig[0].unitePrice);
+$('#itemQtyTxtOrderPage').val(itemDetailsBig[0].qty);
