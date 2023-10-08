784703fb74e5acc513a4e541372b1f30069ba161
punchihewa <navishkapunchihewa66@gmail.com>
2023-10-08 11:23:55 +0530

    update

--- assignments/JS/IN_MEMORY_POS/DB/DB.js
+++ assignments/JS/IN_MEMORY_POS/DB/DB.js
@@ -2,3 +2,9 @@ var CustomerDetails = [
     {cid: "C001", cname: "Dasun", caddress: "Galle", csalary: "1000.22"},
     {cid: "C002", cname: "Iman", caddress: "Mathara", csalary: "2000.22"}
 ];
+
+var itemDetails = [
+    {itemCode:"I001",itemName:"Lenovo",unitePrice: "145.00",qty: 100},
+    {itemCode:"I002",itemName:"Sunlight",unitePrice: "345.00",qty: 150},
+    {itemCode:"I003",itemName:"Mac",unitePrice: "245.00",qty: 400}
+];
