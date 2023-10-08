7a5d4e0f5038b0892e123888506e5cc66776e05d
punchihewa <navishkapunchihewa66@gmail.com>
2023-10-08 21:13:54 +0530

    update

--- assignments/JS/IN_MEMORY_POS/Controller/Home_Controller.js
+++ assignments/JS/IN_MEMORY_POS/Controller/Home_Controller.js
@@ -1,32 +1,34 @@
-let items = document.querySelector("#itemN")
-let cus = document.querySelector("#customerN")
-let home = document.querySelector("#homeN")
-let placeOder = document.querySelector("#plceOderN")
-let table = document.querySelector('#TableN')
+// let items = document.getElementById("#itemN")
+// let cus = document.getElementById("#customerN")
+// let home = document.getElementById("#homeN")
+// let placeOder = document.getElementById("#plceOderN")
+// let table = document.getElementById('#TableN')
 
-document.querySelector("#items").addEventListener("click",function (){
-    cus.style.display='none';
-    placeOder.style.display='none';
-    table.style.display='none';
-    items.style.display='block';
+document.getElementById("items").addEventListener("click",function (){
+    document.getElementById("customerN").style.display='none';
+    document.getElementById("plceOderN").style.display='none';
+    document.getElementById("TableN").style.display='none';
+    document.getElementById("itemN").style.display='block';
+
 })
 
-document.querySelector("#custeR").addEventListener("click",function (){
-    placeOder.style.display='none';
-    items.style.display='none';
-    table.style.display='none';
-    cus.style.display='block';
+document.getElementById("custeR").addEventListener("click",function (){
+    document.getElementById("plceOderN").style.display='none';
+    document.getElementById("TableN").style.display='none';
+    document.getElementById("itemN").style.display='none';
+    document.getElementById("customerN").style.display='block';
 })
-document.querySelector("#placeOders").addEventListener("click",function (){
-    cus.style.display='none';
-    items.style.display='none';
-    placeOder.style.display='block';
-    table.style.display='block';
+document.getElementById("placeOders").addEventListener("click",function (){
+    document.getElementById("plceOderN").style.display='block';
+    document.getElementById("TableN").style.display='block';
+    document.getElementById("itemN").style.display='none';
+    document.getElementById("customerN").style.display='none';
+
 })
-document.querySelector("#home").addEventListener("click",function (){
-    cus.style.display='none';
-    items.style.display='none';
-    placeOder.style.display='none';
-    table.style.display='none';
+document.getElementById("home").addEventListener("click",function (){
+    document.getElementById("plceOderN").style.display='none';
+    document.getElementById("TableN").style.display='none';
+    document.getElementById("itemN").style.display='none';
+    document.getElementById("customerN").style.display='none';
 
 })
