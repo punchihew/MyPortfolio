a11119ff93a46c5dfa131f23f101188efff7d258
punchihewa <navishkapunchihewa66@gmail.com>
2023-10-08 12:13:39 +0530

    update

--- assignments/JS/IN_MEMORY_POS/Controller/Item_Controller.js
+++ assignments/JS/IN_MEMORY_POS/Controller/Item_Controller.js
@@ -70,3 +70,57 @@ $("#itemQtyTxt").keydown(function (e){
 
 
 
+function itemSave(){
+
+    if(regexItemCode.test($("#itemCodeTxt").val()) && regexItemName.test($("#itemNameTxt").val()) && regexItemUnitePrice.test($("#unitePriceTxt").val()) && regexItemQty.test($("#itemQtyTxt").val())){
+        var is = false;
+
+        for(let i = 0; i < itemDetails.length; i++){
+            if(itemDetails[i].itemCode == $("#itemCodeTxt").val()){
+                is = true;
+            }
+        }
+
+        if(is == false){
+
+            var tblBody = $("#itemTblBody");
+
+            var itemCode = $("#itemCodeTxt").val();
+            var itemName = $("#itemNameTxt").val();
+            var unitePrice = $("#unitePriceTxt").val();
+            var qty = $("#itemQtyTxt").val();
+
+            item = {
+                itemCode : itemCode,
+                itemName : itemName,
+                unitePrice : unitePrice,
+                qty : qty
+            }
+
+            itemDetails.push(item);
+
+            let tr=$('<tr> <td>'+itemDetails[itemDetails.length-1].itemCode+'</td> <td>'+itemDetails[itemDetails.length-1].itemName+'</td> <td>'+itemDetails[itemDetails.length-1].unitePrice+'</td> <td>'+itemDetails[itemDetails.length-1].qty+'</td></tr>');
+            $("#itemTblBody").append(tr);
+
+            itemClearFields();
+
+        }else{
+            alert("Item Code Already Used")
+        }
+    }
+
+
+    $("#itemTblBody>tr").click(function (e){
+
+        $("#itemCodeTxt").val($(this).children().eq(0).text());
+        $("#itemNameTxt").val($(this).children().eq(1).text());
+        $("#unitePriceTxt").val($(this).children().eq(2).text());
+        $("#itemQtyTxt").val($(this).children().eq(3).text());
+
+    });
+
+}
+
+
+
+
